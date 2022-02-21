22932b3390b3ed88a9f32995c6212fa1e396f493
nsahsimon <nsahsimon01@gmail.com>
2022-02-21 15:25:30 +0100

    updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,4 +4,4 @@ Color appColor = Colors.red;
 const int maxClientIndex = 500;
 const int maxClientCount = 500;
 const String adminAppVersion = '1.0.2';
-const String testVar = 'Hello';
+const String testVar = 'Hel';
